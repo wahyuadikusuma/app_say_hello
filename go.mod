5adaac8e456e185ecfe075752e34af3e597ae088
Wahyu Adi Kusuma <wahyuadikusuma@gmail.com>
2023-08-30 12:55:21 +0700

    consume go-modules v2

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/wahyuadikusuma/app_say_hello
 go 1.20
 
 require github.com/wahyuadikusuma/go-modules v1.1.1
+
+require github.com/wahyuadikusuma/go-modules/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	say_hello "github.com/wahyuadikusuma/go-modules"
+	say_hello_2 "github.com/wahyuadikusuma/go-modules/v2"
 )
 
 func main() {
 	fmt.Println(say_hello.SayHello("Wahyu"))
+	fmt.Println(say_hello_2.SayHello("Wahyu", "Solo"))
 }
